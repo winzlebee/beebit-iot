1f43ee7bb16730c6cb323a44631fd6cf818c207f
Winfried Holzapfel <wholzapfel@hotmail.com>
2019-10-21 16:10:59 +1100

    Quick path fix

--- scripts/install_raspi.sh
+++ scripts/install_raspi.sh
@@ -19,7 +19,7 @@ pushd opencv-4.1.1-armhf
 sudo cp opencv-4.1.1-arm /opt
 popd
 
-echo "export CPLUS_INCLUDE_PATH=/opt/opencv-4.1.1-arm/include" | sudo tee -a ~/.bashrc
+echo "export CPLUS_INCLUDE_PATH=/usr/local/include:/opt/opencv-4.1.1-arm/include:$CPLUS_INCLUDE_PATH" | sudo tee -a ~/.bashrc
 echo "export LD_LIBRARY_PATH=/opt/opencv-4.1.1-arm/lib" | sudo tee -a ~/.bashrc
 source ~/.bashrc
 
