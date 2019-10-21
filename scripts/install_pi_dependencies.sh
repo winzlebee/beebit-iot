d027e6ddab9b7e0e72ddd34e02305747711dd2e9
Winfried Holzapfel <wholzapfel@hotmail.com>
2019-10-21 16:40:58 +1100

    script fixes

--- scripts/install_pi_dependencies.sh
+++ scripts/install_pi_dependencies.sh
@@ -1,19 +1,18 @@
 # Install all dependencies required by OpenCV
-sudo apt install libcurl4-openssl-dev build-essential libgtk2.0-dev
-sudo apt install libgtk-3-dev libcanberra-gtk3-dev
-sudo apt install libtiff-dev zlib1g-dev
-sudo apt install libjpeg-dev libpng-dev
-sudo apt install libavcodec-dev libavformat-dev libswscale-dev libv4l-dev
-sudo apt install libxvidcore-dev libx264-dev
+sudo apt install libcurl4-openssl-dev build-essential libgtk2.0-dev -y
+sudo apt install libgtk-3-dev libcanberra-gtk3-dev -y
+sudo apt install libtiff-dev zlib1g-dev -y
+sudo apt install libjpeg-dev libpng-dev -y
+sudo apt install libavcodec-dev libavformat-dev libswscale-dev libv4l-dev -y
+sudo apt install libxvidcore-dev libx264-dev -y
 
 mkdir raspi_install && cd raspi_install
 WORKING_DIRECTORY=`pwd`
 
 # Install a precompiled version of OpenCV for the raspberry pi
 git clone git@github.com:winzlebee/opencv-4.1.1-armhf.git
-tar -xvf opencv-4.1.1-armhf.tar.bz2
-
 pushd opencv-4.1.1-armhf
+tar -xvf opencv-4.1.1-armhf.tar.bz2
 sudo cp opencv-4.1.1-arm /opt
 popd
 
@@ -22,7 +21,7 @@ echo "export LD_LIBRARY_PATH=/opt/opencv-4.1.1-arm/lib" | sudo tee -a ~/.bashrc
 source ~/.bashrc
 
 # Install RaspiCam so that the pi camera can be used on the device
-git clone https://github.com/cedricve/raspicam .
+git clone https://github.com/cedricve/raspicam
 mkdir raspicam/build
 pushd raspicam/build
 cmake ../
--- scripts/install_raspi.sh
+++ scripts/install_raspi.sh
@@ -2,6 +2,10 @@
 
 # Install the beebit service onto the system
 sudo mkdir /opt/beebit
+
+# Use the web api to get a new registration UUID for this device. Remember to send JWT token
+
+
 sudo cp -r beetrack *.cfg dnn/ /opt/beebit
 sudo cp scripts/beebit.service /etc/systemd/system/
 systemctl enable beebit
