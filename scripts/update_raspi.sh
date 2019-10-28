c9d43d39b3f7e0d577474eb8f2a1b3fa32486d3a
Winfried Holzapfel <wholzapfel@hotmail.com>
2019-10-28 18:34:20 +1100

    Also copy service files

--- scripts/update_raspi.sh
+++ scripts/update_raspi.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
 # Updates the currently installed version to be this current version
+
+sudo rm /etc/systemd/system/beebit.service
+sudo rm /etc/systemd/system/beenetconf.service
+
+sudo cp scripts/beebit.service /etc/systemd/system/
+sudo cp scripts/beenetconf.service /etc/systemd/system/
+
 sudo rm -r /opt/beebit/*
 sudo cp -r beetrack *.cfg netconf.py dnn/ /opt/beebit
 
